a755ac4fb46b3aa9f7dab8a2d2cd1c9e3db0ee7c
Ivaniuk Aleksandra <ivaniukalekss@gmail.com>
2025-12-08 00:15:26 +0300

    feat: добавление HomeViewModel

---
+++ app/src/main/java/com/ivaniuk/algolearnsimple/presentation/viewmodel/HomeViewModel.kt
@@ -0,0 +1,28 @@
+package com.ivaniuk.algolearnsimple.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ivaniuk.algolearnsimple.domain.repository.AlgorithmRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class HomeViewModel(
+    private val repository: AlgorithmRepository
+) : ViewModel() {
+
+    val algorithms = repository.getAllAlgorithms()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+
+    fun toggleFavorite(algorithmId: Int) {
+        viewModelScope.launch {
+            repository.toggleFavorite(algorithmId)
+        }
+    }
+
+    suspend fun getAlgorithmById(id: Int) = repository.getAlgorithmById(id)
+}
