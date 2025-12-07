fe9c854da45ab3d4d6938bfe55f9ca2081dd0fa6
Ivaniuk Aleksandra <ivaniukalekss@gmail.com>
2025-12-08 00:20:52 +0300

    feat: добавление навигации между экранами

---
+++ app/src/main/java/com/ivaniuk/algolearnsimple/presentation/navigation/AppNavigation.kt
@@ -0,0 +1,52 @@
+package com.ivaniuk.algolearnsimple.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.ivaniuk.algolearnsimple.AppContainer
+import com.ivaniuk.algolearnsimple.presentation.screens.AlgorithmDetailScreen
+import com.ivaniuk.algolearnsimple.presentation.screens.HomeScreen
+import com.ivaniuk.algolearnsimple.presentation.viewmodel.HomeViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun AppNavigation(
+    appContainer: AppContainer
+) {
+    val navController = rememberNavController()
+    val viewModel = HomeViewModel(appContainer.algorithmRepository)
+    val coroutineScope = rememberCoroutineScope()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(
+                viewModel = viewModel,
+                onAlgorithmClick = { algorithmId ->
+                    navController.navigate("algorithm/$algorithmId")
+                },
+                onToggleFavorite = { algorithmId ->
+                    viewModel.toggleFavorite(algorithmId)
+                }
+            )
+        }
+
+        composable("algorithm/{algorithmId}") { backStackEntry ->
+            val algorithmId = backStackEntry.arguments?.getString("algorithmId")?.toIntOrNull()
+            val algorithm by viewModel.algorithms.collectAsState()
+            val currentAlgorithm = algorithm.find { it.id == algorithmId }
+
+            AlgorithmDetailScreen(
+                algorithm = currentAlgorithm,
+                onBack = { navController.navigateUp() },
+                onToggleFavorite = { algorithmId?.let { viewModel.toggleFavorite(it) } }
+            )
+        }
+    }
+}
